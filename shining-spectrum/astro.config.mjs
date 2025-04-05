136844fda17dd3c17015e4bd89c529e31936b1b2
Abdullah Deniz (Privat) <achenbach75@hotmail.de>
2025-04-05 23:01:40 +0200

    Iitialize project with Astro framework

---
+++ shining-spectrum/astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://adeniz75.github.io',
+    base: 'Yip-Yip-Book-Club',
+})
